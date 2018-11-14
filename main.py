08b21c601565008b040983b37bde6879f77873ad
Laukik Kathoke <34758271+Laukik33@users.noreply.github.com>
2018-11-14 05:46:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from wakeonlan import send_magic_packet
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+@app.route("/")
+def index():
+    send_magic_packet('1C.3E.84.14.6A.30')
 
-if __name__ == '__main__':
-  app.run()
+if __name__ =="__main__":
+    app.run(debug=True)
